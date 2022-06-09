e78bc033325e97e73c8f08c8173b75a2eaaf5d36
Luisa Fernanda Betancur Loiaza <lbetancu@emeal.nttdata.com>
2022-06-09 16:54:17 -0500

    subir proyecto principal

---
+++ src/main/java/com/clase/de/rapaso4/clase/de/repaso4/ClaseDeRepaso4Application.java
@@ -0,0 +1,13 @@
+package com.clase.de.rapaso4.clase.de.repaso4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseDeRepaso4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaseDeRepaso4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clase/de/rapaso4/clase/de/repaso4/ClaseDeRepaso4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clase.de.rapaso4.clase.de.repaso4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseDeRepaso4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
